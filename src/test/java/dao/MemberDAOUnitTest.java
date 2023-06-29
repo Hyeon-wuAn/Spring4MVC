12810792ceadd254336f05410e0e90ef42ea4510
Hyeon-Wu An <zerox2519@gmail.com>
2023-06-29 15:16:41 +0900

    MemberDAO loginMember 단위 테스트

--- src/test/java/dao/MemberDAOUnitTest.java
+++ src/test/java/dao/MemberDAOUnitTest.java
@@ -12,6 +12,7 @@ import zero.hello.spring4.dao.MemberDAO;
 import zero.hello.spring4.model.Member;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/servlet-context.xml",
@@ -30,6 +31,17 @@ public class MemberDAOUnitTest {
                 "abc123", "abc123@987xyz.co.kr", null);
         // assertEquals(테스트 메서드, 검증값)
         assertEquals(mdao.insertMember(m) , 1);
+    }
+
+    @Test
+    public void loginMember() throws Exception {
+        Member m = new Member();
+        m.setUserid("abc123");
+        m.setPasswd("987xyz");
+
+
+        // System.out.println(mdao.loginMember(m));
+        assertNotNull(mdao.loginMember(m));
     }
 
 }
