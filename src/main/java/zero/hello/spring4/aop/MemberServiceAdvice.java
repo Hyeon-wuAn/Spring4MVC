00668b22fea262be612b0cc81cf674efe8d4c01c
Hyeon-Wu An <zerox2519@gmail.com>
2023-07-03 17:10:07 +0900

    join에 대해 포인트컷, 어드바이스 추가

--- src/main/java/zero/hello/spring4/aop/MemberServiceAdvice.java
+++ src/main/java/zero/hello/spring4/aop/MemberServiceAdvice.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import zero.hello.spring4.controller.BoardController;
 
@@ -18,15 +19,23 @@ public class MemberServiceAdvice {
 
     private Logger logger = LogManager.getLogger(MemberServiceAdvice.class);
 
-    @Around("execution(* zero.hello.spring4.controller.MemberController.myinfo(..))")
+    @Pointcut("execution(* zero.hello.spring4.controller.MemberController.myinfo(..))")
+    public void myInfoPoint() {
+    }
+
+    @Pointcut("execution(* zero.hello.spring4.controller.MemberController.join(..))")
+    public void joinPoint() {
+    }
+
+    @Around("myInfoPoint()")
     public Object myinfoAOPProcess(ProceedingJoinPoint pjp)
-                                                throws Throwable {
+            throws Throwable {
         logger.info("myinfoAOPProcess 호출!!");
         HttpSession sess = null;
 
         // join point 에서 넘겨준 매개변수에서 원하는 객체 추출
-        for(Object o : pjp.getArgs()){
-            if(o instanceof HttpSession)
+        for (Object o : pjp.getArgs()) {
+            if (o instanceof HttpSession)
                 sess = (HttpSession) o;
         }
 
@@ -40,4 +49,27 @@ public class MemberServiceAdvice {
 
     }
 
+    @Around("joinPoint()")
+    public Object joinAOPProcess(ProceedingJoinPoint pjp)
+            throws Throwable {
+        logger.info("joinAOPProcess 호출!!");
+        HttpSession sess = null;
+
+        // join point 에서 넘겨준 매개변수에서 원하는 객체 추출
+        for (Object o : pjp.getArgs()) {
+            if (o instanceof HttpSession)
+                sess = (HttpSession) o;
+        }
+
+        // 포인트컷 대상 메서드 실행
+        // 로그인 안 했다면 -> join 으로 이동
+        // 로그인 했다면 -> myinfo 로 이동
+        if (sess.getAttribute("member") != null)
+            return "redirect:/member/myinfo";
+
+        //
+        Object obj = pjp.proceed();
+        return obj;
+
+    }
 }
